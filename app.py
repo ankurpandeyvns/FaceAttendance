f2df0745cc3f27fceec3baa46b3ef3b766746fe5
Ankur Pandey <ankurpandeyvns@dtechterminal.local>
2018-06-27 17:41:23 +0530

    1.0.2

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
 from flask import *
-from func import recognizer
-
+#from func import recognizer
+from camera import *
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
+    data = ''
+    try:
+        if request.method == "POST":
+            if request.form.get["click"] == "click":
+                redi('/there')
+    except Exception as e:
+        data = str(e)
     return render_template('index.html')
+
+@app.route('/a')
+def getthere():
+    return 'Working'
+
+def redi(a):
+    a += 'abd'
+    return redirect(a)
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- func.py
+++ func.py
@@ -1,2 +1,12 @@
-def recognizer():
-    return 'recognized'
+from cv2 import *
+import os
+cam = VideoCapture(0)
+s, img = cam.read()
+if s:
+    imshow("cam-test",img)
+    destroyWindow("cam-test")
+    imwrite("./ToCheck/filename.jpg",img)
+    t=os.popen("face_recognition --cpus 2 ./KnownFaces ./ToCheck | cut -d ',' -f2").read()
+    name=t.split()
+    name=str(name[len(name)-1])
+    print (name)
